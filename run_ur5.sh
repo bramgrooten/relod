05f0151de852213b8bfb052226b1e6d9f1a92b43
Bram Grooten <b.j.grooten@student.tue.nl>
2023-09-13 16:57:07 -0600

    run 3 exps

--- run_ur5.sh
+++ run_ur5.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-algo="madi"
-for seed in {48..50}
-do
-python task_ur5_visual_reacher.py --algorithm $algo --batch_size 128 --env_steps 100000 --work_dir "/home/gautham/madi/results/${algo}_clean" --seed $seed
+#algo="madi"
+#for seed in {48..50}
+#do
+#python task_ur5_visual_reacher.py --algorithm $algo --batch_size 128 --env_steps 100000 --work_dir "/home/gautham/madi/results/${algo}_clean" --seed $seed
+#done
+
+
+init_seed=62
+repeats=3
+
+for ((i=0; i<$repeats; i++)); do
+    current_seed=$((init_seed + i))
+    python task_ur5_visual_reacher.py --algorithm 'rad' --background_color 'black' --seed $current_seed
+    python task_ur5_visual_reacher.py --algorithm 'madi' --background_color 'black' --save_mask --seed $current_seed
 done
-z
+
+
