65b69025942f10b98967e88ea62ca31649437b14
Bram Grooten <b.j.grooten@student.tue.nl>
2023-09-15 14:04:35 -0600

    implement env_vid full screen now

--- relod/envs/visual_ur5_min_time_reacher/env_vid.py
+++ relod/envs/visual_ur5_min_time_reacher/env_vid.py
@@ -1,6 +1,8 @@
 import cv2
 import random
 import time
+from screeninfo import get_monitors
+import numpy as np
 
 
 class VideoPlayerWithDot:
@@ -8,15 +10,22 @@ class VideoPlayerWithDot:
         self.video_paths = video_paths
         self.current_video_idx = 0
         self.dot_position = None
-        self.reset_env()
+        self.radius = 80
+        self.margin = 30
+        self.color = (0, 0, 255)  # Red in BGR format
 
         # Setting window properties for fullscreen mode
         cv2.namedWindow('Video with Dot', cv2.WINDOW_NORMAL)
         cv2.setWindowProperty('Video with Dot', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-        # # Get screen dimensions
-        # self.screen_width = cv2.getDisplayProperty('Video with Dot', cv2.WND_PROP_FULLSCREEN_WIDTH)
-        # self.screen_height = cv2.getDisplayProperty('Video with Dot', cv2.WND_PROP_FULLSCREEN_HEIGHT)
+        # Get screen dimensions
+        monitor = get_monitors()[0]  # Assuming you want the primary monitor
+        self.screen_width = monitor.width
+        self.screen_height = monitor.height
+        print(f'Screen resolution (w x h): {self.screen_width}x{self.screen_height}')
+
+        # Set first environment
+        self.reset_env()
 
     def reset_env(self):
         # If cap is already open, release it
@@ -29,9 +38,16 @@ class VideoPlayerWithDot:
         h, w, _ = frame.shape
 
         # Generate a random position for the dot
-        x = random.randint(0, w-1)
-        y = random.randint(0, h-1)
-        self.dot_position = (x, y)
+        x, y = np.random.random(2)
+        self.dot_position = (self.radius + self.margin + x * (w - 2*self.radius - 2*self.margin),
+                             self.radius + self.margin + y * (h - 2*self.radius - 2*self.margin))
+
+        # Compute scaled dot position after resizing the frame
+        original_video_width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        original_video_height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        scaled_dot_x = int(self.dot_position[0] * (self.screen_width / original_video_width))
+        scaled_dot_y = int(self.dot_position[1] * (self.screen_height / original_video_height))
+        self.dot_position = (scaled_dot_x, scaled_dot_y)
 
         # Cycle to the next video
         self.current_video_idx = (self.current_video_idx + 1) % len(self.video_paths)
@@ -50,13 +66,11 @@ class VideoPlayerWithDot:
         slow_down_factor = 1.0  # Increasing this factor will make video play slower
         delay = int(1000 / fps * slow_down_factor)
 
-        # Draw a red dot on the frame
-        radius = 7
-        color = (0, 0, 255)  # Red in BGR format
-        cv2.circle(frame, self.dot_position, radius, color, -1)  # -1 thickness fills the circle
-
         # Resize frame to screen resolution
-        # frame = cv2.resize(frame, (int(self.screen_width), int(self.screen_height)))
+        frame = cv2.resize(frame, (int(self.screen_width), int(self.screen_height)))
+
+        # Draw a red dot on the frame
+        cv2.circle(frame, self.dot_position, self.radius, self.color, -1)  # -1 thickness fills the circle
 
         # Display the frame
         cv2.imshow('Video with Dot', frame)
