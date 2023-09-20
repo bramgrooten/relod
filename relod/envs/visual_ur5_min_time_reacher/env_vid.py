347dbeb0726756001f18b554f6f32a4aca1deaff
Gautham Vasan <gauthamv.529@gmail.com>
2023-09-19 18:27:33 -0600

    Optimize video playback code

--- relod/envs/visual_ur5_min_time_reacher/env_vid.py
+++ relod/envs/visual_ur5_min_time_reacher/env_vid.py
@@ -24,40 +24,42 @@ class VideoPlayer:
 
         # Inits
         self.dot_position = None
-        self.radius = 80
+        self.radius = 84
         self.margin = 30
         self.color = (0, 0, 255)  # Red in BGR format
         self.switch = multiprocessing.Value('i', 0)
-
-    def play_video(self, video_path):
-        cap = cv2.VideoCapture(video_path)
-
+        
+        # Load all videos
+        self.all_videos = []
+        for video_path in self.video_paths:
+            video = []
+            cap = cv2.VideoCapture(video_path)
+            while True:
+                tic = time.time()
+                ret, frame = cap.read()
+                # Check if the video has reached the end
+                if not ret:
+                    break
+                frame = cv2.resize(frame, (self.screen_width, self.screen_height))
+                video.append(frame)
+            self.all_videos.append(video[:])
+            
+            
+    def play_video(self, video_ind):
         # Create a full-screen window
         cv2.namedWindow('Video Playback', cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty('Video Playback', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # Resize the frame to fit the screen resolution
-            frame = cv2.resize(frame, (self.screen_width, self.screen_height))
-
-             # Draw a red dot on the frame
+        for frame in self.all_videos[video_ind]:
+            # Draw a red dot on the frame
             cv2.circle(frame, self.dot_position, self.radius, self.color, -1)  # -1 thickness fills the circle
-
-
+            
             cv2.imshow('Video Playback', frame)
-
             if self._video_switch.value == 1:
                 break
 
             if cv2.waitKey(40) & 0xFF == ord('q'):
                 break
-
-        cap.release()
-        cv2.destroyAllWindows()
 
     def switch_to_next_video(self):
         if self.video_process:
@@ -65,26 +67,29 @@ class VideoPlayer:
             self.video_process.join()
             self._video_switch.value = 0
 
+        # Toggle through videos
+        self.current_video_index = (self.current_video_index + 1) % len(self.video_paths)
+        
         # Generate a random position for the dot
         scaled_dot_x = np.random.randint(low=self.radius, high=self.screen_width-self.radius)
         scaled_dot_y = np.random.randint(low=self.radius, high=self.screen_height-self.radius)
         self.dot_position = (scaled_dot_x, scaled_dot_y)
 
-        # Toggle through videos
-        self.current_video_index = (self.current_video_index + 1) % len(self.video_paths)
-        video_path = self.video_paths[self.current_video_index]
-        self.video_process = multiprocessing.Process(target=self.play_video, args=(video_path,))
+        self.video_process = multiprocessing.Process(target=self.play_video, args=(self.current_video_index,))
         self.video_process.start()
 
     def run(self):
         while True:
             self.switch_to_next_video()
             while not self.switch.value:
-                time.sleep(0.1)
-            self.switch.value = 0
+                time.sleep(0.2)
 
             if self.switch.value == 2:
+                self._video_switch.value = 1
                 break
+            
+            self.switch.value = 0
+            
         print("Done playing videos on the monitor")
             
 
@@ -98,25 +103,3 @@ if __name__ == "__main__":
         if time.time() - tic > 4:
             player.switch.value = 1
             tic = time.time()
-
-
-
-
-# if __name__ == "__main__":
-#     # videos = ['/mnt/c/Users/s136407/Desktop/video17.mp4',]
-#     mode = 'video_hard'
-#     player = VideoPlayerWithDot(mode=mode)
-
-#     start_time = time.time()
-#     quit_pressed = False
-
-#     while not quit_pressed:
-#         quit_pressed = player.play_frame_with_dot()
-
-#         # Check if 2 seconds have passed since the last reset
-#         if time.time() - start_time >= 30:
-#             player.reset_env()
-#             start_time = time.time()
-
-#     player.cap.release()
-#     cv2.destroyAllWindows()
