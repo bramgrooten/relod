99b4977afbbf2c9e5ea36b3773bec742e927dbf1
Gautham Vasan <gauthamv.529@gmail.com>
2023-09-19 18:28:51 -0600

    Train eval switch script

--- run_ur5.sh
+++ run_ur5.sh
@@ -7,25 +7,12 @@
 #done
 
 
-init_seed=258
-repeats=1
+init_seed=300
+repeats=3
 
 for ((i=0; i<$repeats; i++)); do
     current_seed=$((init_seed + i))
-    python task_ur5_visual_reacher.py --algorithm 'madi' --seed $current_seed --work_dir "/home/gautham/madi/results/madi" --description "video-bg-madi" --train_env_mode "video_easy_5" --save_mask --env_steps 1000000
+    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "eval_madi" --seed $current_seed  --init_steps 1000 --device "cuda:0" --save_mask --strong_augment 'conv'
+    current_seed=$((init_seed + i + repeats))
+    python task_ur5_visual_reacher.py --algorithm 'rad' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "eval_madi" --seed $current_seed  --init_steps 1000 --device "cuda:0" --save_mask --strong_augment 'conv'
 done
-
-# init_seed=260
-
-# for ((i=0; i<$repeats; i++)); do
-#     current_seed=$((init_seed + i))
-#     python task_ur5_visual_reacher.py --algorithm 'rad' --seed $current_seed --work_dir "/home/gautham/madi/results/sac_rad" --description "video-bg-sac-rad" --train_env_mode "video_easy_5"
-# done
-
-# init_seed=265
-
-# for ((i=0; i<$repeats; i++)); do
-#     current_seed=$((init_seed + i))
-#     python task_ur5_visual_reacher.py --algorithm 'rad' --seed $current_seed --work_dir "/home/gautham/madi/results/sac_only" --rad_offset 0 --description "video-bg-sac-only" --train_env_mode "video_easy_5"
-# done
-
