implement overlay augmentation and AugmentationRecorder. Also: fix bug in MaDi where masking was done before augmentation!

relod/algo/sac_madi_agent.py
@@ -111,9 +111,10 @@ class MaDiPerformer(BasePerformer):
 
 
 class MaDiLearner(BaseLearner):
-    def __init__(self, args, performer=None) -> None:
+    def __init__(self, args, performer=None, augm_rec=None) -> None:
         self._args = args
         self._args.device = torch.device(args.device)
+        self.augm_rec = augm_rec
 
         if not 'conv' in self._args.net_params:  # no image
             self._args.image_shape = (0, 0, 0)
@@ -253,6 +254,7 @@ class MaDiLearner(BaseLearner):
     
     def _update_critic(self, images, proprioceptions, actions, rewards, next_images, next_proprioceptions, dones):
         with torch.no_grad():
+            next_images = self._performer.apply_mask(next_images)
             _, policy_actions, log_pis, _ = self._actor(next_images, next_proprioceptions)
             target_Q1, target_Q2 = self._critic_target(next_images, next_proprioceptions, policy_actions)
             target_V = torch.min(target_Q1, target_Q2) - self._alpha.detach() * log_pis
@@ -264,10 +266,20 @@ class MaDiLearner(BaseLearner):
 
         if self._args.strong_augment != 'none':
             images_augm = strong_augment(images, self._args.strong_augment)
+            if self.augm_rec is not None:
+                self.augm_rec.record(images, images_augm, self._num_updates)
+                print(f"\n\n aug record called. num upd: {self._num_updates}")
             images = torch.cat([images, images_augm], dim=0)
             proprioceptions = torch.cat([proprioceptions, proprioceptions], dim=0)
             actions = torch.cat([actions, actions], dim=0)
             target_Q = torch.cat([target_Q, target_Q], dim=0)
+
+            images = self._performer.apply_mask(images)
+            if self.augm_rec is not None:
+                self.augm_rec.record(images[:images.shape[0]//2], images[images.shape[0]//2:], self._num_updates, masked=True)
+                print(f"aug rec call again for masked augs and imgs")
+        else:
+            images = self._performer.apply_mask(images)
 
         # get current Q estimates
         current_Q1, current_Q2 = self._critic(images, proprioceptions, actions, detach_encoder=False)
@@ -294,6 +306,8 @@ class MaDiLearner(BaseLearner):
         return critic_stats
 
     def _update_actor_and_alpha(self, images, proprioceptions):
+        images = self._performer.apply_mask(images)
+
         # detach encoder, so we don't update it with the actor loss
         _, pis, log_pis, log_stds = self._actor(images, proprioceptions, detach_encoder=True)
         actor_Q1, actor_Q2 = self._critic(images, proprioceptions, pis, detach_encoder=True)
@@ -343,8 +357,9 @@ class MaDiLearner(BaseLearner):
         if images is not None:
             images = torch.as_tensor(images, device=self._args.device).float()
             next_images = torch.as_tensor(next_images, device=self._args.device).float()
-            images = self._performer.apply_mask(images)
-            next_images = self._performer.apply_mask(next_images)
+            # images = self._performer.apply_mask(images)
+            # next_images = self._performer.apply_mask(next_images)
+            # this was wrong: should wait until it's augmented before applying mask!
         if propris is not None:
             propris = torch.as_tensor(propris, device=self._args.device).float()
             next_propris = torch.as_tensor(next_propris, device=self._args.device).float()

relod/algo/sac_svea_agent.py
@@ -88,9 +88,10 @@ class SVEAPerformer(BasePerformer):
         del self
 
 class SVEALearner(BaseLearner):
-    def __init__(self, args, performer=None) -> None:
+    def __init__(self, args, performer=None, augm_rec=None) -> None:
         self._args = args
         self._args.device = torch.device(args.device)
+        self.augm_rec = augm_rec
 
         if not 'conv' in self._args.net_params: # no image
             self._args.image_shape = (0, 0, 0)
@@ -225,6 +226,8 @@ class SVEALearner(BaseLearner):
 
         if self._args.strong_augment != 'none':
             images_augm = strong_augment(images, self._args.strong_augment)
+            if self.augm_rec is not None:
+                self.augm_rec.record(images, images_augm, self._num_updates)
             images = torch.cat([images, images_augm], dim=0)
             proprioceptions = torch.cat([proprioceptions, proprioceptions], dim=0)
             actions = torch.cat([actions, actions], dim=0)

task_ur5_visual_reacher.py
@@ -9,7 +9,7 @@ import numpy as np
 import relod.utils as utils
 import matplotlib.pyplot as plt
 from relod.logger import Logger
-from relod.video_rec import MaskRecorder
+from relod.video_rec import MaskRecorder, AugmentationRecorder
 from relod.algo.comm import MODE
 from relod.algo.local_wrapper import LocalWrapper
 from relod.algo.sac_rad_agent import SACRADLearner, SACRADPerformer
@@ -97,6 +97,8 @@ def parse_args():
     parser.add_argument('--masker_lr', default=3e-4, type=float)  # was 1e-3 in MaDi work, but 3e-4 is standard here. Can try 1e-3 later
     parser.add_argument('--save_mask', default=False, action='store_true')
     parser.add_argument('--save_mask_freq', default=1000, type=int)
+    parser.add_argument('--save_augm', default=False, action='store_true')
+    parser.add_argument('--save_augm_freq', default=1000, type=int)
     parser.add_argument('--strong_augment', default='none', type=str, help="Augmentations in ['none', 'conv', 'overlay']")
     parser.add_argument('--anneal_masker_lr', default='none', type=str, help="['none', 'cosine']")
     # agent
@@ -221,33 +223,39 @@ def main():
     agent = LocalWrapper(episode_length_step, mode, remote_ip=args.remote_ip, port=args.port)
     agent.send_data(args)
     print(f"Algorithm: {args.algorithm}")
+    mask_rec = None
+    augm_rec = None
+
+    if args.save_augm:
+        args.augm_dir = args.work_dir + '/augmentations'
+        os.makedirs(args.augm_dir, exist_ok=False)
+        augm_rec = AugmentationRecorder(args.augm_dir, args)
+
     if args.algorithm == 'rad':
         agent.init_performer(SACRADPerformer, args)
         agent.init_learner(SACRADLearner, args, agent.performer)
-        mask_rec = None
     elif args.algorithm == 'sac':
         args.rad_offset = 0.0
         print("Running SAC: rad_offset is now set to 0 (overridden if it was not 0 already)")
         agent.init_performer(SACRADPerformer, args)
         agent.init_learner(SACRADLearner, args, agent.performer)
-        mask_rec = None
     elif args.algorithm == 'madi':
         agent.init_performer(MaDiPerformer, args)
-        agent.init_learner(MaDiLearner, args, agent.performer)
+        agent.init_learner(MaDiLearner, args, agent.performer, augm_rec)
         if args.save_mask:
             args.mask_dir = args.work_dir + '/madi_masks'
             os.makedirs(args.mask_dir, exist_ok=False)
             mask_rec = MaskRecorder(args.mask_dir, args)
-        else:
-            mask_rec = None
     elif args.algorithm == 'svea':
         assert args.strong_augment != 'none', 'must specify strong_augment when running svea'
         agent.init_performer(SVEAPerformer, args)
-        agent.init_learner(SVEALearner, args, agent.performer)
-        mask_rec = None
+        agent.init_learner(SVEALearner, args, agent.performer, augm_rec)
     else:
         raise NotImplementedError()
 
+    # For the overlay augmentation
+    if args.strong_augment == 'overlay':
+        assert os.environ['DMCGB_DATASETS'] == "/home/gautham/madi/relod/datasets_augmentation/dmcgb", "Check your location of the Places365 dataset for the overlay augmentation"
 
     # input('go?')
 
@@ -274,8 +282,8 @@ def main():
     print(f'Experiment starts at: {start_time}')
     
     # Eval
-    if args.eval_env_mode != 'none':
-        eval(total_steps, args, agent, env, mask_rec, L)
+    # if args.eval_env_mode != 'none':
+    #     eval(total_steps, args, agent, env, mask_rec, L)
     
     # Save all rewards
     train_rewards = np.zeros((args.env_steps//150, 150))