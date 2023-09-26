6f078806aca7066a4b764ed690f68c098d09a8db
Bram Grooten <b.j.grooten@student.tue.nl>
2023-09-26 15:57:11 -0600

    setup night runs

--- run_ur5.sh
+++ run_ur5.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-#algo="madi"
-#for seed in {48..50}
-#do
-#python task_ur5_visual_reacher.py --algorithm $algo --batch_size 128 --env_steps 100000 --work_dir "/home/gautham/madi/results/${algo}_clean" --seed $seed
-#done
 
+init_seed=1550
+repeats=5
 
-init_seed=1530
-repeats=3
-
-prev_offset=0.04
+#prev_offset=0.04
+#offset=$(echo "2 * $prev_offset" | bc)
+#prev_offset=$offset
 
 for ((i=0; i<$repeats; i++)); do
     current_seed=$((init_seed + i))
-    offset=$(echo "2 * $prev_offset" | bc)
-    prev_offset=$offset
-    python task_ur5_visual_reacher.py --algorithm 'rad' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "rad_offset$offset" --seed $current_seed --rad_offset $offset
+    python task_ur5_visual_reacher.py --algorithm 'rad' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "rad_reruns" --seed $current_seed
+done
 
+
+for ((i=0; i<$repeats; i++)); do
     current_seed=$((init_seed + i + repeats))
-    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "conv_beforemask_noanneal" --seed $current_seed --strong_augment 'conv' --save_augm --save_mask
+    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "overlay_aftermask" --seed $current_seed --strong_augment 'overlay' --when_augm 'after' --save_augm --save_mask
 
-    # current_seed=$((init_seed + i + repeats + repeats))
-    # python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "overlay_beforemask_anneal10k" --seed $current_seed --strong_augment 'overlay' --save_augm --save_mask --anneal_masker_lr 'cosine10k'
-
-    # current_seed=$((init_seed + i + repeats + repeats + repeats))
-    # python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "conv_beforemask_anneal10k" --seed $current_seed --strong_augment 'conv' --save_augm --save_mask --anneal_masker_lr 'cosine10k'
+    current_seed=$((init_seed + i + 2*repeats))
+    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "overlay_b-and-a-mask" --seed $current_seed --strong_augment 'overlay' --when_augm 'both' --save_augm --save_mask
 done
