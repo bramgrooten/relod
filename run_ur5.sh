1dadc7c996a0172da5100f4247d9e712281d59b0
Bram Grooten <bram.grooten@gmail.com>
2023-09-29 10:15:13 -0600

    setup convafter runs

--- run_ur5.sh
+++ run_ur5.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 
-init_seed=1700
+init_seed=1900
 repeats=5
 
 #prev_offset=0.04
 #offset=$(echo "2 * $prev_offset" | bc)
 #prev_offset=$offset
 
+# for ((i=0; i<$repeats; i++)); do
+#     current_seed=$((init_seed + i))
+#     python task_ur5_visual_reacher.py --algorithm 'rad' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "rad_reruns" --seed $current_seed
+# done
+
+
 for ((i=0; i<$repeats; i++)); do
     current_seed=$((init_seed + i))
-    python task_ur5_visual_reacher.py --algorithm 'rad' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "rad_reruns" --seed $current_seed
-done
+    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "conv_aftermask" --seed $current_seed --strong_augment 'conv' --when_augm 'after' --save_augm --save_mask
 
-
-for ((i=0; i<$repeats; i++)); do
-    current_seed=$((init_seed + i + repeats))
-    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "overlay_aftermask" --seed $current_seed --strong_augment 'overlay' --when_augm 'after' --save_augm --save_mask
-
-    current_seed=$((init_seed + i + 2*repeats))
-    python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "overlay_b-and-a-mask" --seed $current_seed --strong_augment 'overlay' --when_augm 'both' --save_augm --save_mask
+    # current_seed=$((init_seed + i + repeats))
+    # python task_ur5_visual_reacher.py --algorithm 'madi' --work_dir "/home/gautham/madi/results" --camera_id 0 --description "overlay_b-and-a-mask" --seed $current_seed --strong_augment 'overlay' --when_augm 'both' --save_augm --save_mask
 done
